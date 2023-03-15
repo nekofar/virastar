09f92e68c08d455d0d9b3b35f96bb470245d121c
Milad Nekofar <milad@nekofar.com>
2023-03-15 16:53:26 +0330

    refactor: improve `normalizeEllipsis` method

--- src/virastar.ts
+++ src/virastar.ts
@@ -724,16 +724,20 @@ export class Virastar {
     return text.replace(/[ \t]*\.{3,}/g, '…')
   }
 
-  normalizeEllipsis(text: string) {
+  /**
+   * Normalizes the usage of ellipsis by replacing more than one ellipsis with one and adding a space after it if needed.
+   * @param text The text to normalize.
+   * @returns The normalized text.
+   */
+  private normalizeEllipsis(text: string): string {
     return (
       text
-
-        // replaces more than one ellipsis with one
+        // Replaces more than one ellipsis with one.
         .replace(/(…){2,}/g, '…')
-
-        // replaces (space|tab|zwnj) after ellipsis with one space
-        // NOTE: allows for space before ellipsis
-        .replace(/([ ]{1,})*…[ \t\u200c]*/g, '$1… ')
+        // Adds a space after ellipsis if needed.
+        .replace(/…([ \t\u200c]*)/g, '… ')
+        // Removes spaces before ellipsis.
+        .replace(/[ \t]+…/g, '…')
     )
   }
 
