9257d7cb1a3db8d72a6a764f61b13b116111baf5
Milad Nekofar <milad@nekofar.com>
2023-03-15 14:20:50 +0330

    refactor: improve `fixSuffixSpacingHamzeh` method

--- src/virastar.ts
+++ src/virastar.ts
@@ -996,17 +996,20 @@ export class Virastar {
     )
   }
 
-  private fixSuffixSpacingHamzeh(text: string) {
+  /**
+   * Replaces heh + ye/hamza with heh-ye + ZWNJ and returns the modified text.
+   * @param text The text to modify.
+   * @returns The modified text with heh + ye/hamza replaced with heh-ye + ZWNJ.
+   */
+  private fixSuffixSpacingHamzeh(text: string): string {
     const replacement = '$1\u0647\u200c\u06cc$3'
+
     return (
       text
-
         // heh + ye
         .replace(/(\S)(ه[\s\u200c]+[یي])([\s\u200c])/g, replacement)
-
         // heh + standalone hamza
         .replace(/(\S)(ه[\s\u200c]?\u0621)([\s\u200c])/g, replacement)
-
         // heh + hamza above
         .replace(/(\S)(ه[\s\u200c]?\u0654)([\s\u200c])/g, replacement)
     )
