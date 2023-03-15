6fa92ef3104079af0295669d08408ce884646925
Milad Nekofar <milad@nekofar.com>
2023-03-15 07:26:41 +0330

    refactor: improve `cleanupExtraMarks` method

--- src/virastar.ts
+++ src/virastar.ts
@@ -982,20 +982,27 @@ export class Virastar {
     )
   }
 
-  cleanupExtraMarks(text: string) {
-    return (
-      text
+  /**
+   * Removes extra marks from the text and replaces repeated marks with a single one.
+   *
+   * @param text The input text to clean up.
+   * @returns The cleaned text.
+   */
+  private cleanupExtraMarks(text: string) {
+    // remove space between different/same marks
+    text = text.replace(/([؟?!])([ ]+)(?=[؟?!])/g, '$1');
 
-        // removes space between different/same marks (combining for cleanup)
-        .replace(/([؟?!])([ ]+)(?=[؟?!])/g, '$1')
+    // replace more than one exclamation mark with just one
+    text = text.replace(/(!){2,}/g, '$1');
 
-        // replaces more than one exclamation mark with just one
-        .replace(/(!){2,}/g, '$1')
-        // replaces more than one english or persian question mark with just one
-        .replace(/([\u061F?]){2,}/g, '$1') // \u061F = `؟`
-        // re-orders consecutive marks
-        .replace(/(!)([ \t]*)([\u061F?])/g, '$3$1')
-    ) // `?!` --> `!?`
+    // replace more than one english or persian question mark with just one
+    text = text.replace(/([\u061F?]){2,}/g, '$1');
+
+    // re-order consecutive marks
+    text = text.replace(/(!)([ \t]*)([\u061F?])/g, '$3$1');
+
+    // `?!` --> `!?`
+    return text;
   }
 
   // replaces kashidas to ndash in parenthetic
